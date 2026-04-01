03bec702bcf6de0d3bf4bdbffe4ff6063af4f2b1
kilik42 <marvinevins@yahoo.com>
2026-03-31 20:35:49 -0500

    Comment out Terraform configuration in test-bucket.tf

--- test-bucket.tf
+++ test-bucket.tf
@@ -1,29 +1,29 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0" # Use latest version if possible
-    }
-  }
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0" # Use latest version if possible
+#     }
+#   }
 
-    backend "s3" {
-      bucket = "marvin-jenkins-tf-state-031726"
-      key    = "jenkins-test-031726.tfstate"
-      region = "us-east-2"
-      encrypt = true
-    }
-}
+#     backend "s3" {
+#       bucket = "marvin-jenkins-tf-state-031726"
+#       key    = "jenkins-test-031726.tfstate"
+#       region = "us-east-2"
+#       encrypt = true
+#     }
+# }
 
-provider "aws" {
-  region  = "us-east-1"
-}
+# provider "aws" {
+#   region  = "us-east-1"
+# }
 
-resource "aws_s3_bucket" "frontend" {
-  bucket_prefix = "jenkins-bucket-"
-  force_destroy = true
+# resource "aws_s3_bucket" "frontend" {
+#   bucket_prefix = "jenkins-bucket-"
+#   force_destroy = true
   
 
-  tags = {
-    Name = "Jenkins Bucket"
-  }
-}
+#   tags = {
+#     Name = "Jenkins Bucket"
+#   }
+# }
